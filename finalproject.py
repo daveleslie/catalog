e5746fcf41d9056ade3fad75d852002c3be8be4b
David <dave.leslie.online@gmail.com>
2015-10-29 12:34:29 +0200

    Add CRUD functionality for Menu Items

--- finalproject.py
+++ finalproject.py
@@ -10,19 +10,19 @@ from database_setup import Base, Restaurant, MenuItem
 #Fake Restaurants
 # restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'},
-               {'name':'Blue Burgers', 'id':'2'},
-               {'name':'Taco Hut', 'id':'3'}]
+# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'},
+#               {'name':'Blue Burgers', 'id':'2'},
+#               {'name':'Taco Hut', 'id':'3'}]
 
-#Fake Menu Items
+# Fake Menu Items
 items = [
     {'name':'Cheese Pizza', 'description':'made with fresh cheese',
      'price':'$5.99','course' :'Entree', 'id':'1'},
     {'name':'Chocolate Cake','description':'made with Dutch Chocolate',
-     'price':'$3.99', 'course':'Dessert','id':'2'},
+     'price':'$3.99', 'course':'Dessert', 'id':'2'},
     {'name':'Caesar Salad', 'description':'with fresh organic vegetables',
      'price':'$5.99', 'course':'Entree','id':'3'},
-    {'name':'Iced Tea', 'description':'with lemon','price':'$.99',
+    {'name':'Iced Tea', 'description':'with lemon', 'price':'$.99',
      'course':'Beverage','id':'4'},
     {'name':'Spinach Dip', 'description':'creamy dip with fresh spinach',
      'price':'$1.99', 'course':'Appetizer','id':'5'}
@@ -97,19 +97,53 @@ def menuList(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 # 6. Add new menu item routing
-@app.route('/restaurants/<int:restaurant_id>/newitem')
+@app.route('/restaurants/<int:restaurant_id>/newitem/', methods=['GET', 'POST'])
 def addMenuItem(restaurant_id):
-    return render_template('newmenuitem.html', restaurant=restaurant)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name = request.form['name'],
+            description = request.form['description'],
+            price = request.form['price'],
+            course = request.form['course'],
+            restaurant_id = restaurant.id
+        )
+        session.add(newItem)
+        session.commit()
+        flash ("New Menu Item Added")
+        return redirect(url_for('menuList', restaurant_id=restaurant.id))
+    else:
+        return render_template('newmenuitem.html', restaurant=restaurant)
 
 # 7. Edit Menu item routing
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return render_template('editmenuitem.html', restaurant=restaurant, item=item)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        item.name = request.form['name']
+        item.price = request.form['price']
+        item.course = request.form['course']
+        item.description = request.form['description']
+        session.add(item)
+        session.commit()
+        flash ("Menu Item Updated")
+        return redirect(url_for('menuList', restaurant_id=restaurant.id))
+    else:
+        return render_template('editmenuitem.html', restaurant=restaurant, item=item)
 
 # 8. Delete Menu Item routing
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return render_template('deletemenuitem.html', restaurant=restaurant, item=item)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        flash ("Menu Item Deleted")
+        return redirect(url_for('menuList', restaurant_id=restaurant.id))
+    else:
+        return render_template('deletemenuitem.html', restaurant=restaurant, item=item)
 
 
 if __name__ == '__main__':
